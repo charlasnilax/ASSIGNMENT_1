f0b8e572dca558205d9cf237eed69d95049e4b78
charlasnilax <charlasvijayakumarnilaxan@gmail.com>
2023-02-14 11:36:30 +0530

    final

--- main.js
+++ main.js
@@ -34,14 +34,15 @@ window.onload = function() {
   const loader = document.getElementById("loader");
   loader.style.display = "none";
 };
+// image zoom out
 function bigImg(x) {
-  x.style.height = "300px";
-  x.style.width = "400px";
+  x.style.height = "250px";
+  x.style.width = "350px";
 }
 
 function normalImg(x) {
-  x.style.height = "200px";
-  x.style.width = "300px";
+  x.style.height = "150px";
+  x.style.width = "250px";
 };
 // grid
 filterSelection("all")
